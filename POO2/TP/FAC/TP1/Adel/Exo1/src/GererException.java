77036e280c4a1fc25c68dbc06f27638e712bec45
Adel2411 <hadjarabadel.2411@gmail.com>
2024-02-23 00:30:36 +0100

    Cours RESAUX section B (introduction)

--- POO2/TP/FAC/TP1/Adel/Exo1/src/GererException.java
+++
@@ -1,8 +0,0 @@
-public class GererException extends Exception {
-    //Constructeur par defaut de la classe gerer exception
-    public GererException(String message) {
-        super(message);
-    }
-
-    
-}
