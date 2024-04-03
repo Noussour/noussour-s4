6bfa9a46f116ef71f9fbc8cf65a933dd87e85937
Adel2411 <hadjarabadel.2411@gmail.com>
2024-04-03 15:30:03 +0100

    TP3 POO2 added some changes

--- POO2/TP/FAC/TP3/Adel/src/Piece.java
+++ POO2/TP/FAC/TP3/Adel/src/Piece.java
@@ -14,12 +14,17 @@ public class Piece {
   public void draw() {
     int count = 1;
     int color = 0;
+    int x;
     for (int i = 0; i < m.length; i++) {
       for (int j = 0; j < m[i].length; j++) {
         if (m[i][j] == count) {
           if (color % 2 == 0) g.setColor(Color.RED);
           else g.setColor(Color.WHITE);
           g.fillOval(j * 100 + 20, i * 100 + 20, 60, 60);
+          g.setColor(Color.BLACK);
+          g.setFont(new Font("Arial", Font.BOLD, 24));
+          x = (count >= 10) ? j * 100 + 34 : j * 100 + 42;
+          g.drawString(Integer.toString(count), x, i * 100 + 60);
           count++;
         }
       }
